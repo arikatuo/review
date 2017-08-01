8ef70c8cae2b13d6a44474f5ab35ba9fa8e68f90
sumimasah <superonlyme@outlook.com>
2017-08-01 19:15:03 +0800

    。

--- review_ssm/src/main/java/com/hy/dao/impl/UserDaoImpl.java
+++ review_ssm/src/main/java/com/hy/dao/impl/UserDaoImpl.java
@@ -2,6 +2,7 @@ package com.hy.dao.impl;
 
 import com.hy.dao.UserDao;
 import com.hy.entity.User;
+import com.hy.mapper.UserMapper;
 
 import javax.jws.soap.SOAPBinding;
 
@@ -9,8 +10,11 @@ import javax.jws.soap.SOAPBinding;
  * Created by dell on 2017/8/1.
  */
 public class UserDaoImpl implements UserDao{
+    private UserMapper userMapper;
+
     @Override
     public User queryById(int id) {
-        return null;
+        User user=userMapper.queryById(id);
+        return user;
     }
 }
